a249efb930a65652f53f644c179734861791f86d
Bpsingh39 <bheeshmpalsingh39@gmail.com>
2021-11-09 20:41:50 +0530

    Conditional Statement...

---
+++ src/conditionalStatement/ConditionalStatements.java
@@ -0,0 +1,22 @@
+package conditionalStatement;
+
+public class ConditionalStatements {
+    public String statements(int age){
+        String message;
+        if(age<10){
+            message="this is a child";
+        }
+        else if(age>10 && age<25){
+            message="this is a young";
+        }
+        else{
+            message="this is old";
+        }
+        return message;
+    }
+    public static void main(String[]args){
+        ConditionalStatements obj=new ConditionalStatements();
+        String result=obj.statements(25);
+        System.out.println(result);
+    }
+}
