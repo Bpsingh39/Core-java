bd6c03f529117b0db339fe87ad4f860eecced8a9
Bpsingh39 <bheeshmpalsingh39@gmail.com>
2021-11-13 16:50:47 +0530

    static keyword

---
+++ src/staticKeyword/StaticKeywordWork.java
@@ -0,0 +1,14 @@
+package staticKeyword;
+
+public class StaticKeywordWork {
+    int a=10; //can not use non static data in static method
+    static int value=5;
+    public static void display(){
+        value=10;
+        System.out.println("print static data :"+value);
+    }
+    public static void main(String[]args){
+        System.out.println("static data member call by clas reference:"+value);
+        StaticKeywordWork.display();
+    }
+}
