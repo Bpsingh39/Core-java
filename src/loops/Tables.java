f2e513f61740689bf7575bcf436be23f9673664a
Bpsingh39 <bheeshmpalsingh39@gmail.com>
2021-11-20 21:09:09 +0530

    table from loop.....

---
+++ src/loops/Tables.java
@@ -0,0 +1,12 @@
+package loops;
+
+public class Tables {
+    public static void main(String[]args){
+        for(int i=1;i<11;i++){
+            int two=2;
+            int result=two*i;
+            System.out.println("TWO'S TABLE :"+result);
+
+        }
+    }
+}
