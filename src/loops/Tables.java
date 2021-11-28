3af50cfb8d8e6082917babb3e8e0299a871a2e5a
Bpsingh39 <bheeshmpalsingh39@gmail.com>
2021-11-28 12:32:39 +0530

    table print using normal way, method, Scanner...

--- src/loops/Tables.java
+++ src/loops/Tables.java
@@ -1,12 +1,33 @@
 package loops;
 
+import java.util.Scanner;
+
 public class Tables {
+    //table print from method/function
+    public void tables(int a){
+        for(int i=1;i<11;i++){
+            int result=a*i;
+            System.out.println("tables that is you want :"+result);
+        }
+
+    }
     public static void main(String[]args){
+        //table print from scanner(give input from console)
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter table number you want to :");
+        int number= scanner.nextInt();
+        for(int i=1;i<11;i++){
+            int result=number*i;
+            System.out.println(result);
+        }
+        //table print normal way
         for(int i=1;i<11;i++){
             int two=2;
             int result=two*i;
             System.out.println("TWO'S TABLE :"+result);
-
         }
+        //to create object for method call
+        Tables obj=new Tables();
+        obj.tables(5);
     }
 }
